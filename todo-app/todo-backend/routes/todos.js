98f47277d6c4a871355bb331cedc1d3585881d71
Joonas Kallio <kallio.joonas@proton.me>
2024-11-11 12:08:55 +0200

    12.8.-12.11. done

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const { Todo } = require("../mongo");
 const router = express.Router();
+const redis = require("../redis");
 
 /* GET todos listing. */
 router.get("/", async (_, res) => {
   const todos = await Todo.find({});
   res.send(todos);
+});
+
+router.get("/statistics", async (req, res) => {
+  const todoCounter = await redis.getAsync("todoCount");
+  const added_todos = todoCounter ? parseInt(todoCounter) : 0;
+  return res.json({ added_todos });
 });
 
 /* POST todo to listing. */
@@ -14,6 +21,11 @@ router.post("/", async (req, res) => {
     text: req.body.text,
     done: false,
   });
+
+  const currentCount = await redis.getAsync("todoCount");
+  const newCount = currentCount ? parseInt(currentCount) + 1 : 1;
+  await redis.setAsync("todoCount", newCount);
+
   res.send(todo);
 });
 
